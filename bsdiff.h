475e99a14d347ca7fc8c34f0c63f3c281e44416e
Adam Hickerson <adam.hickerson@cedarware.com>
2023-07-18 08:48:36 -0700

    Fix header definitions to allow including in C++ code
    - Need to eliminate reserved word "new"

--- bsdiff.h
+++ bsdiff.h
@@ -26,10 +26,10 @@
  */
 
 #ifndef BSDIFF_H
-# define BSDIFF_H
+#define BSDIFF_H
 
-# include <stddef.h>
-# include <stdint.h>
+#include <stddef.h>
+#include <stdint.h>
 
 struct bsdiff_stream
 {
@@ -40,6 +40,6 @@ struct bsdiff_stream
 	int (*write)(struct bsdiff_stream* stream, const void* buffer, int size);
 };
 
-int bsdiff(const uint8_t* old, int64_t oldsize, const uint8_t* new, int64_t newsize, struct bsdiff_stream* stream);
+int bsdiff(const uint8_t* old_buffer, int64_t oldsize, const uint8_t* new_buffer, int64_t newsize, struct bsdiff_stream* stream);
 
 #endif
